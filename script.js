3a4159e90def829557da492b3f437a15be0b628b
Mari64ka <93050097+Mari64ka@users.noreply.github.com>
2021-10-30 13:29:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
 
 themeButton.onclick = function() {
-  page.classList.toggle('light-theme');
   page.classList.toggle('dark-theme');
 };
